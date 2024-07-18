3314c6012c36d70b78965dbbf04c5c6abc2eeb1b
James Kerr <kerr@hey.com>
2024-07-17 21:54:46 -0700

    Add dynamic bits

--- app/javascript/controllers/form_controller.js
+++ app/javascript/controllers/form_controller.js
@@ -1,3 +1,7 @@
 import { Controller } from "@hotwired/stimulus"
 
-export default class extends Controller {}
+export default class extends Controller {
+    refresh() {
+        this.element.querySelector("input[value=refresh]").click()
+    }
+}
